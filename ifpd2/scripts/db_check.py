7e21bfdeccf9c76051a24b699d17ad35175b8c30
Gabriele Girelli <gigi.ga90@gmail.com>
2021-03-05 09:44:35 +0100

    Added sorted check

--- ifpd2/scripts/db_check.py
+++ ifpd2/scripts/db_check.py
@@ -5,25 +5,25 @@
 
 import argparse
 from ifpd2.asserts import enable_rich_assert
+from ifpd2.database2 import DataBase
 from ifpd2.scripts import arguments as ap
+import logging
 import os
+from tqdm import tqdm  # type: ignore
 
 
 def init_parser(subparsers: argparse._SubParsersAction) -> argparse.ArgumentParser:
     parser = subparsers.add_parser(
         "check",
         description="""
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, aspernatur,
-natus. Possimus recusandae distinctio, voluptatem fuga delectus laudantium ut,
-inventore culpa sit amet ullam officiis, tenetur nobis eius vitae dolore.
+Check database integrity.
 """,
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        help="Check integrity of a database.",
+        help="Check database integrity.",
     )
 
-    parser.add_argument(
-        "database", metavar="database", type=str, help="Path to database folder."
-    )
+    parser.add_argument("input", type=str, help="Path to database folder (input).")
+
     parser = ap.add_version_option(parser)
     parser.set_defaults(parse=parse_arguments, run=run)
 
@@ -32,10 +32,23 @@ inventore culpa sit amet ullam officiis, tenetur nobis eius vitae dolore.
 
 @enable_rich_assert
 def parse_arguments(args: argparse.Namespace) -> argparse.Namespace:
-    assert os.path.isdir(args.database), f"database folder not found: {args.database}"
+    assert os.path.isdir(args.input), f"cannot find database folder: '{args.input}'"
+    assert os.path.isfile(
+        os.path.join(args.input, "db.pickle")
+    ), "db.pickle file is missing"
     return args
 
 
 @enable_rich_assert
 def run(args: argparse.Namespace) -> None:
-    raise NotImplementedError
+    DB = DataBase(args.input)
+    for chromosome in DB.chromosome_list:
+        previous_position = -1
+        for record in tqdm(
+            DB.walk_chromosome(chromosome),
+            desc=f"Checking sorting '{chromosome.decode()}'",
+            total=DB.chromosome_recordnos[chromosome],
+        ):
+            assert record["start"] > previous_position
+            previous_position = record["start"]
+    logging.info("That's all! :smiley:")
